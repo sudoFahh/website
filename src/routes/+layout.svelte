7369d1703a47856061223c7d2fcc3baf26c269b0
beigestack <m4.sh@tuta.io>
2026-07-14 22:12:33 +0530

    feat: optimized the .gif (made it a .webm)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,10 +8,12 @@
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 
 <div class="relative min-h-screen w-full bg-[#363635] overflow-hidden">
-	<div
-		class="absolute inset-0 bg-center bg-no-repeat bg-contain opacity-25 pointer-events-none"
-		style="background-image: url('/pyramid.gif'); background-size: 45%;"
-	></div>
+	<div class="absolute inset-0 flex items-center justify-center opacity-25 pointer-events-none">
+		<video autoplay loop muted playsinline preload="auto" class="w-[45%] h-auto">
+			<source src="/pyramid.webm" type="video/webm" />
+			<source src="/pyramid.mp4" type="video/mp4" />
+		</video>
+	</div>
 
 	<div class="relative z-10 w-full min-h-screen">
 		{@render children()}
