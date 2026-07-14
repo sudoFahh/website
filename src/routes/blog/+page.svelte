7c20540489d0a358630fbe690f46e4847fcd2f1a
beigestack <m4.sh@tuta.io>
2026-07-14 16:47:18 +0530

    feat: added blog and kept the link :wink:

---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,37 @@
+<svelte:head>
+	<title>blog</title>
+</svelte:head>
+
+<main class="min-h-screen flex flex-col items-center justify-center text-center p-4 text-white">
+	<h2 class="text-2xl font-[Instrument_Serif] text-[#81E979]">hi gng (blog)</h2>
+	<div class="w-[30%] mt-5">
+		<p>blog</p>
+		<a
+			href="/posts/why-ai-is-good-but-bad.txt"
+			class="italic hover:underline text-[#8FBB99] font-[Jetbrains_Mono]">first post</a
+		>
+		<br />
+		<a
+			href="/posts/me-yapping-about-vehicles.txt"
+			class="italic hover:underline text-[#8FBB99] font-[Jetbrains_Mono]"
+		>
+			second post</a
+		>
+		<br />
+		<a
+			href="/posts/poem-about-a-stray.txt"
+			class="italic hover:underline text-[#8FBB99] font-[Jetbrains_Mono]"
+		>
+			third post</a
+		>
+	</div>
+	<p>btw these may/may not remain .txt files</p>
+	<div class="mt-6 flex gap-4 font-[Jetbrains_Mono]">
+		<a class="italic hover:underline text-[#8FBB99]" href="/">/</a>
+		<a class="italic hover:underline text-[#8FBB99]" href="/about">/about</a>
+		<a class="font-bold italic hover:underline text-[#8FBB99]" href="/blog">/blog</a>
+		<a class="italic hover:underline text-[#8FBB99]" href="/projects">/projects</a>
+		<a class="italic hover:underline text-[#8FBB99]" href="/webrings">sillies</a>
+		<a class="italic hover:underline text-[#8FBB99]" href="/misc">/misc</a>
+	</div>
+</main>
---
+++ src/routes/goodbyesamriddh/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		window.location.href = 'https://sudofahh.github.io/website-old/goodbyesamriddh/';
+	});
+</script>
