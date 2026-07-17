ef3686bad972f5a7427ef1c37fb5154370ee5471
beigestack <m4.sh@tuta.io>
2026-07-17 17:38:48 +0530

    feat: ""added"" misc page and changed edit page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -4,11 +4,24 @@
 
 {#if page.status === 404}
 	<main class="min-h-screen flex flex-col items-center justify-center text-center p-4 text-white">
-		<h1 class="text-2xl font-[Instrument_Serif] text-[#81E979]">coming soon</h1>
-		<a href="/" class="mt-6 italic hover:underline text-[#8FBB99] font-[Jetbrains_Mono]"
-			>go back home</a
-		>
+		<h1 class="text-2xl font-[Instrument_Serif] text-[#81E979]">404 (page not found)</h1>
+		<a href="/" class="mt-7 link">go back home</a>
+		<p class="text-[9px] font-[Jetbrains_Mono] mt-7">
+			if you think this is a mistake, please <a href="mailto:m4.sh@tuta.io" class="link"
+				>contact me</a
+			>
+		</p>
 	</main>
 {:else}
-	<p>{page.status}: {page.error?.message}</p>
+	<main class="min-h-screen flex flex-col items-center justify-center text-center p-4 text-white">
+		<h1 class="text-2xl font-[Instrument_Serif] text-[#81E979]">
+			{page.status}: {page.error?.message}
+		</h1>
+		<a href="/" class="mt-6 link">go back home</a>
+		<p class="text-[9px] font-[Jetbrains_Mono] mt-6">
+			if you think this is a mistake, please <a href="mailto:m4.sh@tuta.io" class="link"
+				>contact me</a
+			>
+		</p>
+	</main>
 {/if}
---
+++ src/routes/misc/+page.svelte
@@ -0,0 +1,6 @@
+<main class="min-h-screen flex flex-col items-center justify-center text-center p-4 text-white">
+	<h1 class="text-2xl font-[Instrument_Serif] text-[#81E979]">nothing here (yet)</h1>
+	<a href="/" class="mt-6 italic hover:underline text-[#8FBB99] font-[Jetbrains_Mono]"
+		>go back home</a
+	>
+</main>
